dd1946fe67657a99c79d3cc9f1a8c4718b72021f
chaveiro <nchaveiro@gmail.com>
2013-09-01 01:22:11 +0100

    Original LG sources

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Aug 31 18:43:23 WEST 2013"
+#define LINUX_COMPILE_BY "user"
+#define LINUX_COMPILE_HOST "vmubuntu"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-188) "
